79243e79f45fdee7fdaa90e694799f7bd68ae985
dthisner <dthisner@pm.me>
2024-07-23 16:36:43 +0200

    At lesson 33. Component State Navigation

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,7 +8,7 @@ class Main extends Component {
     constructor() {
         super()
         this.state = {
-            posts: Posts
+            posts: []
         }
         this.removePhoto = this.removePhoto.bind(this)
     }
@@ -21,6 +21,19 @@ class Main extends Component {
         }))
     }
 
+    componentDidMount() {
+        const data = SimulateFetchFromDB()
+        this.setState({
+            posts: data
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("componentDidUpdate")
+        console.log(prevState.posts)
+        console.log(this.state.posts)
+    }
+
     render() {
         return (
             <div>
@@ -31,4 +44,31 @@ class Main extends Component {
     }
 }
 
+function SimulateFetchFromDB() {
+    return Posts
+}
+
 export default Main
+
+/*
+url("https://image.flaticon.com/icons/svg/60/60740.svg") center no-repeat;
+
+The following methods are called when a component is being added to the DOM:
+
+constructor() : called before component is mounted. NEVER put side effect code inside of the constructor. 
+Use ComponentDidMount for that instead. Commonly used to initialize state or bind methods. 
+
+componentWillMount(): invoked immediately before mounting occurs. Called before render. 
+Once again, DO NOT put any side effect code inside of componentWillMount, and do not make 
+API calls in this method  
+
+render(): never fetch data inside of render. Should only be used to return elements.
+
+componentDidMount(): Perfect place to fetch data. It gets called after render. 
+This makes it clear that the initial state is empty at first, until we fetch it and re-trigger render. 
+This forces us to set up our initial state properly, otherwise you're likely to get undefined states.
+The following methods are called when a component is re-rendered to the DOM
+
+componentDidUpdate(): called when the state of a component changes. Perfect place to 
+update UI or make network calls based on previous state before update, and current state
+ */
